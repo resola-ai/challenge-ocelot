bcc3a17fa0adcc355dda7c5ab14922dea6328041
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2023-11-13 14:17:17 +0700

    Allow to createsuperuser with email

--- apps/books/models.py
+++ apps/books/models.py
@@ -1,11 +1,39 @@
 # models.py
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import UserManager as DjangoUserManager
 from django.core import validators
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractBaseUser):
+class UserManager(DjangoUserManager):
+    """Adjusted user manager that works w/o `username` field."""
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a user with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create superuser instance (used by `createsuperuser` cmd)."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(PermissionsMixin, AbstractBaseUser):
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
@@ -28,7 +56,11 @@ class User(AbstractBaseUser):
         max_length=80,
         blank=True,
     )
-
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
     objects = UserManager()
 
 
